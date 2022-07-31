d2f4d3cdfaf9aa2ae944daec1d362f1263bc492a
david.g <david.g@kakaopaysec.com>
2022-07-31 21:25:28 +0900

    [ch_01] 어노테이션 추가

--- ch_01/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
+++ ch_01/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -3,10 +3,12 @@ package com.hello.hellospring.service;
 import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
